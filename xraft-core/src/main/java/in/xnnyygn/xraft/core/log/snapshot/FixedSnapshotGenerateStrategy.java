ec3b7bcf4e8868449689d50746856c4ded8697a7
chen.zhao <xnnyygn@gmail.com>
2018-08-18 08:00:22 +0900

    add doc

--- xraft-core/src/main/java/in/xnnyygn/xraft/core/log/snapshot/FixedSnapshotGenerateStrategy.java
+++ xraft-core/src/main/java/in/xnnyygn/xraft/core/log/snapshot/FixedSnapshotGenerateStrategy.java
@@ -1,9 +1,21 @@
 package in.xnnyygn.xraft.core.log.snapshot;
 
+/**
+ * Threshold based strategy.
+ * <p>
+ * If last applied log index - first log index in file &gt; threshold, generate snapshot.
+ * </p>
+ */
 public class FixedSnapshotGenerateStrategy implements SnapshotGenerateStrategy {
 
     private final int threshold;
 
+    /**
+     * Create.
+     *
+     * @param threshold threshold
+     * @throws IllegalArgumentException if threshold is negative
+     */
     public FixedSnapshotGenerateStrategy(int threshold) {
         if (threshold < 0) {
             throw new IllegalArgumentException("threshold < 0");
