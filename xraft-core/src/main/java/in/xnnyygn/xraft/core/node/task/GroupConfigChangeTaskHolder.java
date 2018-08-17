658d9fbbf7f561cf5bb37980375fb1f197cece59
chen.zhao <xnnyygn@gmail.com>
2018-08-17 13:44:05 +0900

    add test

--- xraft-core/src/main/java/in/xnnyygn/xraft/core/node/task/GroupConfigChangeTaskHolder.java
+++ xraft-core/src/main/java/in/xnnyygn/xraft/core/node/task/GroupConfigChangeTaskHolder.java
@@ -4,13 +4,11 @@ import in.xnnyygn.xraft.core.log.entry.AddNodeEntry;
 import in.xnnyygn.xraft.core.log.entry.GroupConfigEntry;
 import in.xnnyygn.xraft.core.log.entry.RemoveNodeEntry;
 import in.xnnyygn.xraft.core.node.FixedResultGroupConfigTaskReference;
-import in.xnnyygn.xraft.core.rpc.message.AppendEntriesResultMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeoutException;
 
-// TODO add test
 public class GroupConfigChangeTaskHolder {
 
     private static final Logger logger = LoggerFactory.getLogger(GroupConfigChangeTaskHolder.class);
@@ -27,7 +25,7 @@ public class GroupConfigChangeTaskHolder {
     }
 
     public void awaitDone(long timeout) throws TimeoutException, InterruptedException {
-        if(timeout == 0) {
+        if (timeout == 0) {
             reference.getResult();
         } else {
             reference.getResult(timeout);
@@ -43,16 +41,16 @@ public class GroupConfigChangeTaskHolder {
     }
 
     public boolean onLogCommitted(GroupConfigEntry entry) {
-        if(isEmpty()) {
+        if (isEmpty()) {
             return false;
         }
-        logger.debug("log committed, current task {}", task);
-        if(entry instanceof AddNodeEntry && task instanceof AddNodeTask
+        logger.debug("log {} committed, current task {}", entry, task);
+        if (entry instanceof AddNodeEntry && task instanceof AddNodeTask
                 && task.isTargetNode(((AddNodeEntry) entry).getNewNodeEndpoint().getId())) {
             task.onLogCommitted();
             return true;
         }
-        if(entry instanceof RemoveNodeEntry && task instanceof RemoveNodeTask
+        if (entry instanceof RemoveNodeEntry && task instanceof RemoveNodeTask
                 && task.isTargetNode(((RemoveNodeEntry) entry).getNodeToRemove())) {
             task.onLogCommitted();
             return true;
