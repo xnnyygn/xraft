529d7824d7e612e49151432ca7863009871de350
chen.zhao <xnnyygn@gmail.com>
2018-08-22 13:58:30 +0900

    add todo

--- xraft-core/src/main/java/in/xnnyygn/xraft/core/rpc/message/AppendEntriesResultMessage.java
+++ xraft-core/src/main/java/in/xnnyygn/xraft/core/rpc/message/AppendEntriesResultMessage.java
@@ -9,6 +9,7 @@ public class AppendEntriesResultMessage {
 
     private final AppendEntriesResult result;
     private final NodeId sourceNodeId;
+    // TODO remove rpc, just lastEntryIndex required, or move to replicating state?
     private final AppendEntriesRpc rpc;
 
     public AppendEntriesResultMessage(AppendEntriesResult result, NodeId sourceNodeId, @Nonnull AppendEntriesRpc rpc) {
