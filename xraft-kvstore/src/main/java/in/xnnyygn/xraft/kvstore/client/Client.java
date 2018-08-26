b01497c784384fb27ab1807e2ad25abcca1e0d7c
chen.zhao <xnnyygn@gmail.com>
2018-08-26 11:17:38 +0900

    upgrade version

--- xraft-kvstore/src/main/java/in/xnnyygn/xraft/kvstore/client/Client.java
+++ xraft-kvstore/src/main/java/in/xnnyygn/xraft/kvstore/client/Client.java
@@ -8,7 +8,7 @@ import in.xnnyygn.xraft.kvstore.message.SetCommand;
 
 public class Client {
 
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.1";
 
     private final ServerRouter serverRouter;
 
